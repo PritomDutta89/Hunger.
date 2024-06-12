4d3cd0a897ed2338655d30fabf950404703203fe
Pritom Dutta <pritom@ayekart.com>
2024-06-12 10:53:30 +0530

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Cart from "./pages/cart/Cart";
+import PlaceOrder from "./pages/placed-order/PlaceOrder";
 
 const App = () => {
   return (
     <div className="p-0 box-border min-h-[100vh] text-inherit w-[90%] m-auto">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/order" element={<PlaceOrder/>}/>
+      </Routes>
     </div>
   );
 };
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+// import { assets } from "../../assets/assets";
+
+const Header = () => {
+  return (
+    <>
+      <div className="img w-full h-[30rem] rounded-xl mt-2">
+        <div className="">
+          <h2>Order your favorite food here</h2>
+          <p>
+            Discover a world of flavors with our extensive selection of
+            delectable dishes. From hearty mains to tantalizing desserts, our
+            diverse menu offers something to satisfy every craving and delight
+            every palate. Dive into a culinary adventure today!
+          </p>
+          <button>
+            View Menu
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
